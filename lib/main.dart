830befbc66e5734d92b82d618faa7d1c6371dfe9
bedirhan-1 <bedirhan9828@gmail.com>
2025-07-13 21:49:35 +0300

    feat: :art: added theme mode provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/providers/theme_provider.dart';
 import 'package:meals_app/screens/category_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(App());
+  WidgetsFlutterBinding.ensureInitialized();
+  final themeProvider = ThemeProvider();
+
+  runApp(ChangeNotifierProvider.value(value: themeProvider, child: App()));
 }
 
 class App extends StatelessWidget {
@@ -11,16 +16,28 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromARGB(255, 29, 120, 157),
-          brightness: Brightness.dark,
-        ),
-        textTheme: GoogleFonts.latoTextTheme(),
-      ),
-      home: const CategoryScreen(),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 29, 120, 157),
+              brightness: Brightness.light,
+            ),
+            textTheme: GoogleFonts.latoTextTheme(),
+          ),
+          darkTheme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 29, 120, 157),
+              brightness: Brightness.dark,
+            ),
+            textTheme: GoogleFonts.latoTextTheme(),
+          ),
+          themeMode: themeProvider.themeMode,
+          home: const CategoryScreen(),
+        );
+      },
     );
   }
 }
